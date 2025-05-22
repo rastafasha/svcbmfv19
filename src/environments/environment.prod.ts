258912c641f2aa2c7e94d5c93fa62f46cf8c08bc
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-05-22 13:00:16 -0400

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://svcbmf.com/backend-api-svcbmf/public/api/',
+  imageUrl: 'https://svcbmf.com/backend-api-svcbmf/storage/app/public/',
+  // baseUrl: 'http://localhost:8888/proyectos/CI3Server/svcbmf/server/',
+  // imageUrl: 'http://localhost:8888/proyectos/CI3Server/svcbmf/server/media/images/'
+};
